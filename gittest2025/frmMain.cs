c5a36db108eeaad1b0fa2a398790b3bca0a57902
Ayato <Ayato@TKD-NOTE>
2025-05-26 11:45:33 +0900

    プロジェクト ファイルを追加します。

---
+++ gittest2025/frmMain.cs
@@ -0,0 +1,15 @@
+namespace gittest2025
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+    }
+}
